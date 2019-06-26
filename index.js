b7b1121992c9cfc5fafa1e2fdca9c711438a7761
Neha Patel <neha.r.p@gmail.com>
2019-06-25 23:11:02 -0700

    WIP nav dropdown was able to change display of nav bar to hamburger at tablet media query

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 function navDrop(tabletView){
     if(tabletView.matches) {
         nav.style.display = "none";
+        menuBtn.style.display = "inline-block";
     } else {
         nav.style.display ="";
+        menuBtn.style.display = "";
     }
 }
 
@@ -11,4 +13,6 @@ let tabletView = window.matchMedia("(max-width: 800px)");
 tabletView.addListener(navDrop);
 
 let nav = document.querySelector('.nav-items');
-console.log(nav);
+// console.log(nav);
+let menuBtn = document.querySelector('.menuBtn');
+console.log(menuBtn);
