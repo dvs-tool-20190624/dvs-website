841aa44fbcdd9226383c478ce3faed1039607e0d
Neha Patel <neha.r.p@gmail.com>
2019-06-26 10:08:22 -0700

    added navigation media queries across all pages. About us is not displaying the hamburger for some reason...

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
-function navDrop(tabletView){
-    if(tabletView.matches) {
-        nav.style.display = "none";
-        menuBtn.style.display = "inline-block";
+// function navDrop(tabletView){
+//     if(tabletView.matches) {
+//         nav.style.display = "none";
+//         menuBtn.style.display = "inline-block";
 
-        
-    } else {
-        nav.style.display ="";
-        menuBtn.style.display = "";
-    }
-}
+
+//     } else {
+//         nav.style.display ="";
+//         menuBtn.style.display = "";
+//     }
+// }
 
 const toggleMenu = () => {
     menuItems.classList.toggle('menu--open');
 }
 
-let tabletView = window.matchMedia("(max-width: 800px)");
-tabletView.addListener(navDrop);
+// let tabletView = window.matchMedia("(max-width: 800px)");
+// tabletView.addListener(navDrop);
 
-let nav = document.querySelector('.nav-items');
-// console.log(nav);
+// let nav = document.querySelector('.nav-items');
+// // console.log(nav);
 
 const menuItems = document.querySelector('.menu-items');
 // console.log(menuItems);
